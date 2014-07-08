84f903c126fb181a013c23eb9a36148b2215ac24
justinalberta <justinbatesalberta@gmail.com>
2014-07-07 22:00:11 -0600

    Revised GUI code to create different panels

--- QuoteManagerView.py
+++ QuoteManagerView.py
@@ -21,38 +21,39 @@ import QuoteManagerController
 from functools import partial
 import config
 
-class Frame(wx.Frame):
 
-    def __init__(self,parent,id):
-        self.uploadScreen(parent,id)
+##    def onSwitchPanels(self, event):
+##        if self.uploadScreen.IsShown():
+##            self.SetTitle("Upload")
+##            self.uploadScreen.Hide()
+##            self.mainScreen.Show()
+##        else:
+##            self.SetTitle("Panel One Showing")
+##            self.uploadScreen.Show()
+##            self.mainScreen.Hide()
+##        self.Layout()
 
-    def uploadScreen(self,parent,id):
-        wx.Frame.__init__(self,parent,id, 'Quote Manager', size =(500,500))
-        uploadPanel=wx.Panel(self)
-        status = self.CreateStatusBar()
-        self.menuFunc(parent,id)
+class uploadPanel(wx.Panel):
+
+    def __init__(self,parent):
+        wx.Panel.__init__(self, parent=parent)
 
         def onSelect(event): #gets option for upload type
             selection = event.GetString()                                                  #<-------------------- Need to get var to
             config.selection = selection
 
         uploadOptions = ['New Quote','Part List','Customer List','Supplier List']
-        uploadCombo = wx.ComboBox(uploadPanel, pos=(20, 10), choices=uploadOptions,style=wx.CB_READONLY)
+        uploadCombo = wx.ComboBox(self, pos=(20, 10), choices=uploadOptions,style=wx.CB_READONLY)
         uploadCombo.Bind(wx.EVT_COMBOBOX, onSelect)
 
-##        selectFileButton = wx.Button(uploadPanel,label="Select File",pos=(20,40))
-##        selectFileButton.Bind(wx.EVT_BUTTON, self.fileFind)
-        def upLoadIt(self):
+        def upLoadIt(event):
             fileFinder = fileFind() #Run by Upload button. If no file is selected, then print message and return.
             if fileFinder ==  None:
-                wx.StaticText(uploadPanel,-1,"No file selected",(40,100))
+                wx.StaticText(self,-1,"No file selected",(40,100))
                 return
             else:
                 QuoteManagerController.uploadQuote(fileFinder,config.selection)
-                wx.StaticText(uploadPanel,-1,"Upload Complete",(40,100))
-
-
-
+                wx.StaticText(self,-1,"Upload Complete",(40,100))
 
         def fileFind(): #Select file to be uploaded, default csv files
             wildcard = "CSV Files (*.csv;)|*.csv|" \
@@ -60,32 +61,58 @@ class Frame(wx.Frame):
             fileFind = wx.FileDialog(self,message="Choose a file",defaultFile="",wildcard=wildcard,style=wx.OPEN)
             if fileFind.ShowModal()== wx.ID_OK:
                 path = fileFind.GetPath()
-                wx.StaticText(uploadPanel,-1,"File Selected: " + path,(40,80))
+                wx.StaticText(self,-1,"File Selected: " + path,(40,80))
                 return(path)
             else:
                 fileFind.Destroy
             fileFind.Destroy
 
-        uploadButton = wx.Button(uploadPanel,label="Upload",pos=(20, 40))
+        uploadButton = wx.Button(self,label="Upload",pos=(20, 40))
         uploadButton.Bind(wx.EVT_BUTTON, upLoadIt)
         #uploadButton.Bind(wx.EVT_BUTTON, partial(QuoteManagerController.uploadQuote, path ))         #filePath = 'C:\\Users\\JB\\Desktop\\QuoteManager\\test.csv'
+        self.Show()
+
+    def Quit(self,event):
+            self.Close()
 
 
+class mainPanel(wx.Panel):
+    def __init__(self,parent):
+        wx.Panel.__init__(self, parent=parent)
+        mainButton = wx.Button(self,label="Main",pos=(20, 40))
 
-    def menuFunc(self,parent,id):
+
+class menuStart(wx.Frame):
+
+    def Quit(self,event):
+        self.Close()
+
+
+    def __init__(self,parent,id):
+        wx.Frame.__init__(self, parent, wx.ID_ANY,"Main")
+
+        uploadScreen = uploadPanel(self)
+
+        uploadScreen.Show()
+        def hideR(event):
+            uploadScreen.Hide()
+            self.showR()
+
         menuBar = wx.MenuBar()
-
         menuFile= wx.Menu()
         menuLookup = wx.Menu()
         menuMaint = wx.Menu()
 
-        menuFile.Append(wx.NewId(),"Exit","Exit Program")
+        exitItem = menuFile.Append(wx.ID_EXIT,"Exit","Exit Program")
+        self.Bind(wx.EVT_MENU, self.Quit,exitItem)
         menuFile.Append(wx.NewId(),"Comment Generator","Create Comment Report")
 
         menuLookup.Append(wx.NewId(),"Query","Lookup Quote") #bind?
         menuLookup.Append(wx.NewId(),"Edit Part","Change Quote") #bind?
 
-        menuMaint.Append(wx.NewId(),"Upload","Upload New Quote")
+        uploadItem = menuMaint.Append(wx.NewId(),"Upload","Upload New Quote")
+
+        self.Bind(wx.EVT_MENU, hideR,uploadItem)
         menuMaint.Append(wx.NewId(),"Archive","Archive Expired Quote")
         menuMaint.Append(wx.NewId(),"Delete","Delete Quote")
 
@@ -93,21 +120,21 @@ class Frame(wx.Frame):
         menuBar.Append(menuLookup,"Lookup")
         menuBar.Append(menuMaint,"Maintenance")
 
+        status = self.CreateStatusBar()
         self.SetMenuBar(menuBar)
 
 
 
-
-
-
-##def __init__(self,parent,id):
-##        wx.Frame.__init__(self,parent,id, 'This is frame', size =(300,300))
-##        panel2=wx.Panel(self)
-
-
+    def showR(self):
+        mainf=mainPanel(self)
+        mainf.Show()
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(mainf, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+        self.Layout()
 
 if __name__== '__main__':
     app=wx.App()
-    frame = Frame(parent=None,id=1)
+    frame = menuStart(parent=None,id=1)
     frame.Show()
     app.MainLoop()
